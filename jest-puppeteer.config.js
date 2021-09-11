feeacbc0fcca5884923971b2ffe15c7a5a3104eb
keka3avr <kekazavr228@gmail.com>
2021-09-11 20:37:49 +1100

    try to fix ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     launch: {
         headless: process.env.HEADLESS !== 'false',
-        slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
-        devtools: true,
+        // slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
+        // devtools: true,
     },
     server: {
         command: 'yarn start --port 9002',
